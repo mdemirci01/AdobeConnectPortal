4ea169c261bbb188a29d64a13a2e20ac6006d1d1
mdemirci01 <mdemirci@outlook.com>
2016-11-04 12:35:35 +0300

    theme files

---
+++ AdobeConnectPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdobeConnectPortal.Startup))]
+namespace AdobeConnectPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
